97bfa5e62bbe466381c54e9708ec04ebd6d7a822
lucas <lucasdmandrade@gmail.com>
2023-12-03 17:12:17 -0300

    fix readme

--- src/utils/sortBooks/sortBooks.test.ts
+++ src/utils/sortBooks/sortBooks.test.ts
@@ -21,7 +21,7 @@ describe("sortBooks", () => {
   });
 
   it("should sort books by author in descending order", () => {
-    const expectedOrder = [2, 1, 3, 0];
+    const expectedOrder = [2, 1, 3, 0]; //[4, 2, 1, 3]
 
     const options: SortingOptions = {
       attribute: "author",

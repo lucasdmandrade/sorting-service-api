6a56f1266e91e16ca913a6b2e030958d44f72608
lucas <lucasdmandrade@gmail.com>
2023-12-03 19:12:32 -0300

    fix test and compose

--- src/utils/sortBooks/sortBooks.test.ts
+++ src/utils/sortBooks/sortBooks.test.ts
@@ -21,7 +21,7 @@ describe("sortBooks", () => {
   });
 
   it("should sort books by author in descending order", () => {
-    const expectedOrder = [3, 1, 0, 2]; //[2, 1, 3, 0]
+    const expectedOrder = [3, 1, 0, 2]; //alleged scenario of the test document [2, 1, 3, 0]
 
     const options: SortingOptions = {
       attribute: "author",
